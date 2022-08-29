9a029e587a170a2c3278a038d7797c124d50a246
ebru-dikmen <ebrudikmen00@gmail.com>
2022-08-29 17:38:19 +0300

    register component is implemented.

--- src/app/register/register.component.spec.ts
+++ src/app/register/register.component.spec.ts
@@ -1,5 +1,25 @@
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of, subscribeOn } from 'rxjs';
+import { BoardComponent } from '../board/board.component';
+import { LoginComponent } from '../login/login.component';
+import { ApiService } from '../services/api/api.service';
+import { AuthService } from '../services/auth/auth.service';
 import { RegisterComponent } from './register.component';
+
+let mockAuthSerice: AuthService = {
+  save: () => null,
+  get: () => 'result',
+  clear: () => undefined
+};
+
+const fakeApiService = jasmine.createSpyObj<ApiService>(
+  'ApiService', {
+  postRequest: of(null)
+});
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
@@ -7,8 +27,21 @@ describe('RegisterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule.withRoutes(
+          [
+            { path: 'login', component: LoginComponent },
+            { path: 'board', component: BoardComponent }
+          ]
+        )
+      ],
       declarations: [
-        RegisterComponent
+        RegisterComponent,
+        NgForm
+      ],
+      providers: [
+        { provide: AuthService, useValue: mockAuthSerice },
+        { provide: ApiService, useValue: fakeApiService }
       ]
     }).compileComponents();
 
@@ -19,5 +52,47 @@ describe('RegisterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should fired ngOnInit function', () => {
+    spyOn(component, 'ngOnInit').and.callThrough();
+    component.ngOnInit();
+    expect(component.ngOnInit).toHaveBeenCalled();
+  });
+
+  it('should fired onSubmit function-1', () => {
+    spyOn(component, 'onSubmit').and.callThrough();
+    const form = { value: { userName: 'ebru', password: 'kader ' } } as any;
+    fakeApiService.postRequest.and.returnValue(of({ token: "12345" }));
+    component.onSubmit(form);
+    expect(component.onSubmit).toHaveBeenCalled();
+  });
+
+  it('should fired onSubmit function-2', () => {
+    spyOn(component, 'onSubmit').and.callThrough();
+    const form = { value: { userName: 'ebru', password: 'kader ' } } as any;
+    fakeApiService.postRequest.and.returnValue(of(null));
+    component.onSubmit(form);
+    expect(component.onSubmit).toHaveBeenCalled();
+  });
+
+  it('should fired onSubmit function-3', () => {
+    spyOn(component, 'onSubmit').and.callThrough();
+    const form = { value: { userName: 'ebru', password: 'kader ' } } as any;
+    fakeApiService.postRequest.and.returnValue(of({ message: "Error" }));
+    component.onSubmit(form);
+    expect(component.onSubmit).toHaveBeenCalled();
+  });
+
+  it('should fired checkPlayerAlreadyLogin function', () => {
+    spyOn(component, 'checkPlayerAlreadyLogin').and.callThrough();
+    component.checkPlayerAlreadyLogin();
+    expect(component.checkPlayerAlreadyLogin).toHaveBeenCalled();
+  });
+
+  it('should fired goToLoginPage function', () => {
+    spyOn(component, 'goToLoginPage').and.callThrough();
+    component.goToLoginPage();
+    expect(component.goToLoginPage).toHaveBeenCalled();
   });
 });
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api/api.service'
+import { AuthService } from '../services/auth/auth.service'
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,8 +10,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  constructor(
+    public _api: ApiService,
+    public _auth: AuthService,
+    public _router: Router
+  ) { }
 
-  constructor() { }
+  ngOnInit() {
+    // check if the player is already login when the component is opened
+    this.checkPlayerAlreadyLogin();
+  }
 
-  ngOnInit(): void { }
+  // form submit action to register player
+  onSubmit(form: NgForm) {
+
+    // send HTTP POST request
+    this._api.postRequest('/register', form.value).subscribe((res: any) => {
+      if (res) {
+
+        // check response has token
+        if (res.token) {
+
+          // save player data into localStorage
+          let player = form.value;
+          let playerJson = JSON.stringify(player)
+          this._auth.save('player', playerJson);
+
+          // save token into localStorage
+          this._auth.save('token', res.token);
+
+          // nagivate to board page
+          this._router.navigate(['board']);
+
+          // check error response has a message
+        } else if (res.message) {
+
+          // give alert with error message from response
+          alert(res.message)
+        }
+      } else {
+
+        // give alert with standard error
+        alert('Error!')
+      }
+    });
+  }
+
+  // check the player is already login
+  checkPlayerAlreadyLogin() {
+    
+    // if localStorage has player data, navigate to board page
+    if (this._auth.get('player') != null) {
+      this._router.navigate(['board']);
+    }
+  }
+
+  // nagivate to login page
+  goToLoginPage() {
+    this._router.navigate(['login']);
+  }
 }
