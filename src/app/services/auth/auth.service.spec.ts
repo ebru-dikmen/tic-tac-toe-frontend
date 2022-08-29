a8433d1605f4b07b8daa055d7d856abf33464de8
ebru-dikmen <ebrudikmen00@gmail.com>
2022-08-29 17:19:56 +0300

    auth service is implemented.

--- src/app/services/auth/auth.service.spec.ts
+++ src/app/services/auth/auth.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
+
   let service: AuthService;
 
   beforeEach(() => {
@@ -12,4 +13,25 @@ describe('AuthService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should fire to save function', (() => {
+    spyOn(service, 'save').and.callThrough();
+    const key = "key";
+    const value = "value"
+    service.save(key, value);
+    expect(service.save).toHaveBeenCalled()
+  }));
+
+  it('should fire to get function', (() => {
+    spyOn(service, 'get').and.callThrough();
+    const key = "key";
+    service.get(key);
+    expect(service.get).toHaveBeenCalled()
+  }));
+
+  it('should fire to clear function', (() => {
+    spyOn(service, 'clear').and.callThrough();
+    service.clear();
+    expect(service.clear).toHaveBeenCalled()
+  }));
 });
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -6,4 +6,19 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+  // save an item as key-value pair into localStorage
+  save(key: string, value: string) {
+    localStorage.setItem(key, value);
+  }
+
+  // get item from localStorage using key
+  get(key: string) {
+    return localStorage.getItem(key);
+  }
+
+  // remove all items in localStorage
+  clear() {
+    localStorage.clear();
+  }
 }
