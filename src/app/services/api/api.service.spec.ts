fbb9c1f6ab928d5eb501ae96cc2c1d8b93408938
ebru-dikmen <ebrudikmen00@gmail.com>
2022-08-29 17:23:24 +0300

    api service is implemented.

--- src/app/services/api/api.service.spec.ts
+++ src/app/services/api/api.service.spec.ts
@@ -1,15 +1,32 @@
 import { TestBed } from '@angular/core/testing';
 import { ApiService } from './api.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ApiService', () => {
+
   let service: ApiService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [
+        ApiService,
+      ]
+    });
     service = TestBed.inject(ApiService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should fire to postRequest function', (() => {
+    spyOn(service, 'postRequest').and.callThrough();
+    const path = "/login";
+    const data = {} as any;
+    service.postRequest(path, data);
+    expect(service.postRequest).toHaveBeenCalled()
+  }));
 });
--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map } from 'rxjs/operators';
+import { Observable, of, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  // backend REST API URL
+  private baseUrl: string = 'http://localhost:3535';
+
+  constructor(private _http: HttpClient) { }
+
+  // send an HTTP POST request with path and data
+  postRequest(path: any, data: any): Observable<any> {
+
+    // generate URL
+    let url = this.baseUrl + path;
+
+    return this._http.post(url, data).pipe(
+
+      // map response data
+      map(res => res),
+
+      // catch error
+      // if the response has not ok status, the method does not act as an error to get the error message
+      catchError(err => {
+        if (err.status != 200 && err.error && err.error.message) {
+          const errorResponse = {
+            message: err.error.message
+          }
+          return of(errorResponse);
+        } else {
+          return throwError(err);
+        }
+      })
+    );
+  }
 }
