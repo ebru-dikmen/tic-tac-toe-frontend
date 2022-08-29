123863fba03680f76fbf0e6fae064c1495802881
ebru-dikmen <ebrudikmen00@gmail.com>
2022-08-29 16:19:23 +0300

    game logic is implemented.

--- src/app/board/board.component.spec.ts
+++ src/app/board/board.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { CellComponent } from '../cell/cell.component';
 import { BoardComponent } from './board.component';
 
 describe('BoardComponent', () => {
@@ -8,8 +10,13 @@ describe('BoardComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        BoardComponent
-      ]
+        BoardComponent,
+        CellComponent
+      ],
+      imports: [
+        BrowserAnimationsModule,
+        NoopAnimationsModule,
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(BoardComponent);
@@ -31,7 +38,10 @@ describe('BoardComponent', () => {
   it('should fire to startNewGame function', () => {
     spyOn(component, 'startNewGame').and.callThrough();
     component.cells = Array(9);
+    component.isWin = false;
     component.xIsNext = true;
+    component.counter = 0;
+    component.gameOver = false;
     component.startNewGame();
     expect(component.startNewGame).toHaveBeenCalled();
   });
@@ -58,11 +68,43 @@ describe('BoardComponent', () => {
 
   it('should fire to makeMove function-2', (() => {
     spyOn(component, 'makeMove').and.callThrough();
+    component.isWin = true;
+    component.counter = 9;
     component.makeMove(0);
     expect(component.makeMove).toHaveBeenCalled();
   }));
 
-  it('should fire to player function', () => {
+  it('Button click event using spyon', () => {
+    spyOn(component, 'onHide').and.callThrough();
+    component.onHide();
+    expect(component.onHide).toHaveBeenCalled();
+  });
+
+  it('should  fire to  computeToWin function', () => {
+    spyOn(component, 'computeToWin').and.callThrough();
+    const cells = [
+      ["x", "x", "x"],
+      ["o", "x", "x"],
+      ["o", "o", "x"],
+      ["o", "x", "o"],
+    ]
+    cells.forEach(cell => {
+      component.cells = cell;
+      component.computeToWin();
+      expect(component.computeToWin).toHaveBeenCalled();
+    });
+  });
+
+  it('should  fire to  onHide function', () => {
+    spyOn(component, 'onHide').and.callThrough();
+    component.isWin = false;
+    component.counter = 0;
+    component.gameOver = false;
+    component.onHide();
+    expect(component.onHide).toHaveBeenCalled();
+  });
+
+  it('should  fire to  player function', () => {
     spyOn(component, 'player').and.callThrough();
     component.player();
     expect(component.player).toHaveBeenCalled();
--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -3,12 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
-  styleUrls: ['./board.component.scss']
+  styleUrls: ['./board.component.scss'],
 })
 export class BoardComponent implements OnInit {
 
   cells: string[] = [];
   xIsNext: boolean | undefined;
+  isWin: true | false | undefined;
+  counter = 0;
+  gameOver: boolean | undefined;
+  winner: string | null | undefined;
 
   constructor() { }
 
@@ -20,7 +24,11 @@ export class BoardComponent implements OnInit {
   // new game button action
   startNewGame() {
     this.cells = Array(9);
+    this.isWin = false;
+    this.counter = 0;
     this.xIsNext = true;
+    this.gameOver = false;
+    this.winner = null;
   }
 
   // change player state
@@ -31,22 +39,71 @@ export class BoardComponent implements OnInit {
   // apply player move in board
   makeMove(idx: any) {
 
-    // apply player move
-    if (!this.cells[idx]) {
+    // check whether player can play
+    if (!this.isWin && this.counter <= 9) {
 
-      // get current player state
-      let currentPlayer = this.player();
+      // apply player move
+      if (!this.cells[idx]) {
 
-      // prevent apply already done move
-      if (currentPlayer === this.cells[idx]) {
-        return;
+        // get current player state
+        let currentPlayer = this.player();
+
+        // prevent apply already done move
+        if (currentPlayer === this.cells[idx]) {
+          return;
+        }
+
+        // increase score
+        this.counter++;
+
+        // update board
+        this.cells.splice(idx, 1, currentPlayer);
+
+        // change player
+        this.xIsNext = !this.xIsNext;
       }
 
-      // update board
-      this.cells.splice(idx, 1, currentPlayer);
+      // check whether player wins
+      this.isWin = this.computeToWin();
 
-      // change player
-      this.xIsNext = !this.xIsNext;
+      // check whether there is no more move
+      if (this.counter === 9) {
+        this.gameOver = true;
+      }
     }
+  }
+
+  // check whether game is end or continue
+  computeToWin() {
+
+    // win conditions
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    // check each win conditions with actual board values
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+
+      // compare the current condition exists in win conditions and find winner
+      if (this.cells[a] && this.cells[a] === this.cells[b] && this.cells[a] === this.cells[c]) {
+        this.winner = this.cells[a];
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  // close button action for win and game over modal
+  onHide() {
+    this.startNewGame();
   }
 }
